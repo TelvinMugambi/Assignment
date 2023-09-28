8125b3abe62db1060e2035b7b00c8da170b29709
unknown <telvin.muriuki@strathmore.edu>
2023-09-29 00:04:28 +0300

    Uploaded css

--- index.php
+++ index.php
@@ -11,26 +11,34 @@ class home{
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap"
+      rel="stylesheet"
+    />
 </head>
 <body>
-<form action="process/signup_process.php" method="POST">
-    Name <input type="text" name="fullname" id="fullname"><br><br>
-    Email <input type="email" name="email" id="email"><br><br>
-    Password <input type="password" name="password" id="password"><br><br>
-    Confirm Password <input type="password" name="c_password" id="c_password"><br><br>
-    Country <select name="country" id="country">
-        <?php
-        $countries = array("Kenya","Uganda","Tanzania");
-        foreach($countries as $country){
-            
-        ?>
-        <option value="<?php echo "{$country}"?>"><?php echo "{$country}"?></option>
-        <?php
-        }
-        ?>
-    </select><br><br>
-    <input type="submit" name = "signup" value="Sign Up">
-</form>
+    <div class = "signup-box">
+        <form action="process/signup_process.php" method="POST">
+            <h1>WELCOME</h1>
+            <label for="">Name</label>
+            <input type="text" name="fullname" id="fullname"placeholder = "Enter your name">
+            <label for="">Email</label>
+            <input type="email" name="email" id="email"placeholder = "Enter your email address">
+            <label for="">Password</label>
+            <input type="password" name="password" id="password"placeholder = "Enter your password">
+            <label for=""> Confirm Password</label>
+            <input type="password" name="c_password" id="c_password"placeholder = "Repeat password">
+            <label for="">Country</label>
+            <input type="text" name="country" id="country"placeholder = "Type in your country">
+            <input type="submit" name = "signup" value="Sign Up">
+        </form>    
+        <p>
+        By clicking the Sign Up button,you agree to our <br />
+            <a href="#">Terms and Condition</a> and <a href="#">Policy Privacy
+        </p>
+    </div>        
+        
 </body>
 </html>
 <?php
---
+++ viewusers.php
@@ -0,0 +1,61 @@
+<?php
+require_once "db/conn.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users Table</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap"
+      rel="stylesheet"
+    />
+</head>
+<body>
+    
+    <table class = "content-table" >
+        <thead>
+            <tr>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Country</td>
+            </tr>
+        </thead>
+    
+    
+
+
+<?php
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+    parent::__construct($it, self::LEAVES_ONLY);
+    }
+    
+    function beginChildren() {echo "<tbody><tr>";} 
+
+    function current() {return "<td>" . parent::current(). "</td>";}
+
+    function endChildren() {echo "</tr></tbody>" . "\n";} 
+    
+}
+
+try {
+
+  $stmt = $conn->prepare("SELECT name, email, country FROM users ORDER BY name ASC");
+  $stmt->execute();
+
+  // set the resulting array to associative
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+    echo $v;
+  }
+} catch(PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+$conn = null;
+?>
+    </table>
+</body>
+</html>
