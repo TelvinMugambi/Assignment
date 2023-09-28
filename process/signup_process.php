7147c8a4dcd29156e227a5c70c118360cb399679
unknown <telvin.muriuki@strathmore.edu>
2023-09-28 19:33:17 +0300

    Added password and email validation

--- process/signup_process.php
+++ process/signup_process.php
@@ -8,14 +8,23 @@ if(isset($_POST["signup"])){
         $password = $_POST["password"];
         $c_password = $_POST["c_password"];
         $country = $_POST["country"];
+    
+        if(!strcmp($password, $c_password) == 0){
+        die("Error: passwords do not match");
+        }   
 
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            die("Error: Invalid email");
+        } 
+
+        $en_password = password_hash($c_password,PASSWORD_DEFAULT);
         
-        $sql = "INSERT INTO users(name, email, password, country) VALUES (:name, :email, :password, :country)";
+        $sql = "INSERT INTO users(name, email, password, country) VALUES (:name, :email, :en_password, :country)";
         $stmt = $conn->prepare($sql);
 
         $stmt->bindParam(':name', $name, PDO::PARAM_STR);
         $stmt->bindParam(':email', $email, PDO::PARAM_STR);
-        $stmt->bindParam(':password', $password, PDO::PARAM_STR);
+        $stmt->bindParam(':en_password', $en_password, PDO::PARAM_STR);
         $stmt->bindParam(':country', $country, PDO::PARAM_STR);
 
         if($stmt->execute()){
@@ -27,9 +36,5 @@ if(isset($_POST["signup"])){
     }catch(PDOException $e){
         echo "Error: " .$e->getMessage();
     }
-        // $stmt = $pdo->prepare($sql);
-        // $pdo->execute($data);
-    
-
-    
+        
 }
