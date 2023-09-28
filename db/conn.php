bdb131369ddfb4510793c7b2d2a6967e1b7f7e44
unknown <telvin.muriuki@strathmore.edu>
2023-09-28 11:49:47 +0300

    Database connection established

--- db/conn.php
+++ db/conn.php
@@ -3,7 +3,7 @@ require_once "constants.php";
 $Constants = new db_constants("localhost","root","");
 
 try {
-    $conn = new PDO("mysql:host={$Constants->get_server()};dbname=myDB", $Constants->get_user(), $Constants->get_pass());
+    $conn = new PDO("mysql:host={$Constants->get_server()};dbname=assignment_II", $Constants->get_user(), $Constants->get_pass());
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
